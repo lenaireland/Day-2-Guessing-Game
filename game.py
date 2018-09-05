643a93a0272fbf7fcee60d397098cc145431c622
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-05 16:18:57 -0700

    Cleaned up while loop syntax

--- game.py
+++ game.py
@@ -11,31 +11,10 @@ def number_guess():
 
     i = 0
 
-    # while True:
-    #     i += 1
-    #     guess = int(input('Guess my number between 1 and 100! '))
-    #     # if guess != number:
-
-    #     # if guess is not between 1 and 100, then..0
-    #         # give error message, please enter valid number
-    #     # (else) if valid number:
-    #         # 
-
-    #     if guess > number:
-    #         print('Your guess is too high, try again')
-    #         #i += 1
-    #     elif guess < number:
-    #         print('Your guess is too low, try again')
-    #         #i += 1
-    #     else:
-    #         print('Well done, {}! You found my number in {} tries'.format(playername,i))
-    #         break
-
-
     guess = int()
-    while not guess == number:
+    while guess != number:
         i += 1
-        # guess = int(input('Guess my number between 1 and 100! '))
+
         while True:
             try:
                 guess = int(input('Guess my number between 1 and 100! '))
@@ -52,6 +31,5 @@ def number_guess():
     print('Well done, {}! You found my number in {} tries'.format(playername,i))
 
 
-
 number_guess()
-# Put your code here
+
