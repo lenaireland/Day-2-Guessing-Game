ff80ff9e7894237f1cbbdbc54e80f42b0f570abc
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-05 12:45:31 -0700

    Working Guessing Game

--- game.py
+++ game.py
@@ -9,5 +9,19 @@ def number_guess():
     number = randint(1,100)
     print(number)
 
+    i = 1
+    while True:
+        guess = int(input('Guess my number... '))
+        # if guess != number:
+        if guess > number:
+            print('Your guess is too high, try again')
+            i += 1
+        elif guess < number:
+            print('Your guess is too low, try again')
+            i += 1
+        else:
+            print('Well done, {}! You found my number in {} tries'.format(playername,i))
+            break
+
 number_guess()
 # Put your code here
